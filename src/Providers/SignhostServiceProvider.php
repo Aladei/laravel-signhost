bb3d006164bcff0fe0e87836071eee71f6dad7fe
Jeroen de Boer | Aladei <jeroen@aladei.com>
2026-01-22 15:44:03 +0100

    Refactored for aladei/tenacy package. Moved migrations to tenants directory. Removed functionality for publishing migrations.

--- src/Providers/SignhostServiceProvider.php
+++ src/Providers/SignhostServiceProvider.php
@@ -15,31 +15,14 @@ use Noardcode\LaravelSignhost\Console\Commands\FakeSignhostWebhookCommand;
 class SignhostServiceProvider extends ServiceProvider
 {
     /**
-     * The migrations that should be published.
-     *
-     * @var array
-     */
-    protected array $migrations = [
-        'create_sh_transactions_table.php',
-        'create_sh_transaction_files_table.php',
-        'create_sh_transaction_file_links_table.php',
-        'create_sh_transaction_receivers_table.php',
-        'create_sh_transaction_activities_table.php',
-    ];
-
-    /**
      * Perform post-registration booting of services.
      *
      * @return void
      */
     public function boot(): void
     {
-        if ($this->app->runningInConsole()) {
-            $this->consoleFunctionality();
-        }
-
         // Tenancy
-        Tenancy::addMigrationPath(__DIR__.'/../database/migrations/tenants');
+        Tenancy::addMigrationPath(__DIR__.'/../../database/migrations/tenants');
 
         $this->loadRoutesFrom($this->getPackageBasePath('/routes/web.php'));
         $this->mergeConfigFrom($this->getPackageBasePath('/config/disks.php'), 'filesystems.disks');
@@ -58,49 +41,6 @@ class SignhostServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom($this->getPackageBasePath('/config/signhost.php'), 'signhost');
-    }
-
-    /**
-     * Module functionality only available from the console.
-     *
-     * @return void
-     */
-    protected function consoleFunctionality(): void
-    {
-        $timeStamp = Carbon::now();
-        foreach ($this->migrations as $migrationFilename) {
-            if ($this->migrationFileExists($migrationFilename)) {
-                continue;
-            }
-
-            $timeStamp->addSecond();
-            $this->publishes([
-                $this->getPackageBasePath('database/migrations/'.$migrationFilename) => database_path('migrations/'.$timeStamp->format('Y_m_d_His').'_'.$migrationFilename),
-            ], ['signhost-migrations', 'signhost-assets']);
-        }
-
-        $this->publishes([
-            $this->getPackageBasePath('/config/signhost.php') => config_path('signhost.php'),
-        ], ['signhost-config', 'signhost-assets']);
-    }
-
-    /**
-     * Check if the migration file already exists in the migrations directory.
-     *
-     * @param  string  $migrationFilename
-     * @return bool
-     */
-    protected function migrationFileExists(string $migrationFilename): bool
-    {
-        $len = strlen($migrationFilename);
-
-        foreach (glob(database_path('migrations/*.php')) as $filename) {
-            if ((substr($filename, -$len) === $migrationFilename)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     /**
